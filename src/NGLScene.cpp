e411e7ee89ae276aa5c10bb3592636c7d6fa96f8
jmacey <jmacey@bournemouth.ac.uk>
2022-11-03 21:11:40 +0000

    updated for NGL9 renamed master branch main

--- src/NGLScene.cpp
+++ src/NGLScene.cpp
@@ -8,57 +8,55 @@
 #include <ngl/SimpleVAO.h>
 #include <ngl/ShaderLib.h>
 
-
 //----------------------------------------------------------------------------------------------------------------------
-NGLScene::NGLScene(int _timer, QWidget *_parent ) : QOpenGLWidget(_parent )
+NGLScene::NGLScene(int _timer, QWidget *_parent) : QOpenGLWidget(_parent)
 {
   // set this widget to have the initial keyboard focus
   setFocus();
   // re-size the widget to that of the parent (in this case the GLFrame passed in on construction)
   this->resize(_parent->size());
-	m_timerValue=_timer;
-	startSimTimer();
+  m_timerValue = _timer;
+  startSimTimer();
 }
 
 // This virtual function is called once before the first call to paintGL() or resizeGL(),
-//and then once whenever the widget has been assigned a new QGLContext.
+// and then once whenever the widget has been assigned a new QGLContext.
 // This function should set up any required OpenGL context rendering flags, defining display lists, etc.
 
 //----------------------------------------------------------------------------------------------------------------------
 void NGLScene::initializeGL()
 {
-ngl::NGLInit::initialize();
-glClearColor(0.4f, 0.4f, 0.4f, 1.0f);			   // Grey Background
-// enable depth testing for drawing
- glEnable(GL_DEPTH_TEST);
- // Now we will create a basic Camera from the graphics library
- // This is a static camera so it only needs to be set once
- // First create Values for the camera position
- ngl::Vec3 from(0,0,7);
- ngl::Vec3 to(0,0,0);
- ngl::Vec3 up(0,1,0);
- 
- m_view=ngl::lookAt(from,to,up);
- // set the shape using FOV 45 Aspect Ratio based on Width and Height
- // The final two are near and far clipping planes of 0.5 and 10
- m_project=ngl::perspective(45.0f,720.0f/576.0f,0.5f,150.0f);
+  ngl::NGLInit::initialize();
+  glClearColor(0.4f, 0.4f, 0.4f, 1.0f); // Grey Background
+                                        // enable depth testing for drawing
+  glEnable(GL_DEPTH_TEST);
+  // Now we will create a basic Camera from the graphics library
+  // This is a static camera so it only needs to be set once
+  // First create Values for the camera position
+  ngl::Vec3 from(0, 0, 7);
+  ngl::Vec3 to(0, 0, 0);
+  ngl::Vec3 up(0, 1, 0);
 
- ngl::VAOPrimitives::createSphere("sphere",1.0,20);
- ngl::ShaderLib::use(ngl::nglDiffuseShader);
- ngl::ShaderLib::setUniform("Colour",1.0f,1.0f,0.0f,1.0f);
- ngl::ShaderLib::setUniform("lightPos",1.0f,1.0f,1.0f);
- ngl::ShaderLib::setUniform("lightDiffuse",1.0f,1.0f,1.0f,1.0f);
+  m_view = ngl::lookAt(from, to, up);
+  // set the shape using FOV 45 Aspect Ratio based on Width and Height
+  // The final two are near and far clipping planes of 0.5 and 10
+  m_project = ngl::perspective(45.0f, 720.0f / 576.0f, 0.5f, 150.0f);
 
+  ngl::VAOPrimitives::createSphere("sphere", 1.0, 20);
+  ngl::ShaderLib::use(ngl::nglDiffuseShader);
+  ngl::ShaderLib::setUniform("Colour", 1.0f, 1.0f, 0.0f, 1.0f);
+  ngl::ShaderLib::setUniform("lightPos", 1.0f, 1.0f, 1.0f);
+  ngl::ShaderLib::setUniform("lightDiffuse", 1.0f, 1.0f, 1.0f, 1.0f);
 }
 
 //----------------------------------------------------------------------------------------------------------------------
-//This virtual function is called whenever the widget has been resized.
+// This virtual function is called whenever the widget has been resized.
 // The new size is passed in width and height.
-void NGLScene::resizeGL( int _w, int _h )
+void NGLScene::resizeGL(int _w, int _h)
 {
-  m_project=ngl::perspective(45.0f, static_cast<float>( _w ) / _h, 0.05f, 350.0f );
-  m_win.width  = static_cast<int>( _w * devicePixelRatio() );
-  m_win.height = static_cast<int>( _h * devicePixelRatio() );
+  m_project = ngl::perspective(45.0f, static_cast<float>(_w) / _h, 0.05f, 350.0f);
+  m_win.width = static_cast<int>(_w * devicePixelRatio());
+  m_win.height = static_cast<int>(_h * devicePixelRatio());
 }
 
 void NGLScene::loadMatricesToShader()
@@ -68,29 +66,27 @@ void NGLScene::loadMatricesToShader()
   ngl::Mat4 MVP;
   ngl::Mat3 normalMatrix;
   ngl::Mat4 M;
-  M=m_transform.getMatrix();
-  MV=m_view *m_mouseGlobalTX*M;
-  MVP=m_project*MV;
-  normalMatrix=MV;
+  M = m_transform.getMatrix();
+  MV = m_view * m_mouseGlobalTX * M;
+  MVP = m_project * MV;
+  normalMatrix = MV;
   normalMatrix.inverse().transpose();
-  ngl::ShaderLib::setUniform("MVP",MVP);
-  ngl::ShaderLib::setUniform("normalMatrix",normalMatrix);
+  ngl::ShaderLib::setUniform("MVP", MVP);
+  ngl::ShaderLib::setUniform("normalMatrix", normalMatrix);
 }
 
 void NGLScene::loadMatricesToColourShader()
 {
   ngl::ShaderLib::use(ngl::nglColourShader);
   ngl::Mat4 MVP;
-  MVP = m_project*m_view *
-        m_mouseGlobalTX*
+  MVP = m_project * m_view *
+        m_mouseGlobalTX *
         m_transform.getMatrix();
-  ngl::ShaderLib::setUniform("MVP",MVP);
-
+  ngl::ShaderLib::setUniform("MVP", MVP);
 }
 
-
 //----------------------------------------------------------------------------------------------------------------------
-//This virtual function is called whenever the widget needs to be painted.
+// This virtual function is called whenever the widget needs to be painted.
 // this is our main drawing routine
 void NGLScene::paintGL()
 {
@@ -100,59 +96,53 @@ void NGLScene::paintGL()
   // Rotation based on the mouse position for our global
   // transform
   // Rotation based on the mouse position for our global transform
-  ngl::Mat4 rotX;
-  ngl::Mat4 rotY;
-  // create the rotation matrices
-  rotX.rotateX(m_win.spinXFace);
-  rotY.rotateY(m_win.spinYFace);
+  auto rotX = ngl::Mat4::rotateX(m_win.spinXFace);
+  auto rotY = ngl::Mat4::rotateY(m_win.spinYFace);
   // multiply the rotations
-  m_mouseGlobalTX=rotY*rotX;
+  m_mouseGlobalTX = rotY * rotX;
   // draw spring lines
   ngl::ShaderLib::use(ngl::nglColourShader);
   // get our position values and put in a vector
   std::vector<ngl::Vec3> points(2);
-  points[0]=m_spring->getAPosition();
-  points[1]=m_spring->getBPosition();
-  ngl::ShaderLib::setUniform("Colour",1.0f,1.0f,1.0f,1.0f);
+  points[0] = m_spring->getAPosition();
+  points[1] = m_spring->getBPosition();
+  ngl::ShaderLib::setUniform("Colour", 1.0f, 1.0f, 1.0f, 1.0f);
   m_transform.reset();
   loadMatricesToColourShader();
   // load transform stack
-  std::unique_ptr<ngl::AbstractVAO> vao(ngl::VAOFactory::createVAO("simpleVAO",GL_LINES));
+  std::unique_ptr<ngl::AbstractVAO> vao(ngl::VAOFactory::createVAO("simpleVAO", GL_LINES));
   vao->bind();
-  vao->setData(ngl::AbstractVAO::VertexData(2*sizeof(ngl::Vec3),points[0].m_x));
+  vao->setData(ngl::AbstractVAO::VertexData(2 * sizeof(ngl::Vec3), points[0].m_x));
   vao->setNumIndices(2);
-  vao->setVertexAttributePointer(0,3,GL_FLOAT,0,0);
+  vao->setVertexAttributePointer(0, 3, GL_FLOAT, 0, 0);
   vao->draw();
   vao->unbind();
 
-
-
   ngl::ShaderLib::use(ngl::nglDiffuseShader);
 
-  ngl::ShaderLib::setUniform("Colour",1.0f,0.0f,0.0f,1.0f);
-  m_transform.setScale(0.1f,0.1f,0.1f);
+  ngl::ShaderLib::setUniform("Colour", 1.0f, 0.0f, 0.0f, 1.0f);
+  m_transform.setScale(0.1f, 0.1f, 0.1f);
   m_transform.setPosition(m_spring->getAPosition());
   loadMatricesToShader();
   // draw the cube
   ngl::VAOPrimitives::draw("cube");
 
-
-  ngl::ShaderLib::setUniform("Colour",0.0f,1.0f,0.0f,1.0f);
-  m_transform.setScale(0.1f,0.1f,0.1f);
+  ngl::ShaderLib::setUniform("Colour", 0.0f, 1.0f, 0.0f, 1.0f);
+  m_transform.setScale(0.1f, 0.1f, 0.1f);
   m_transform.setPosition(m_spring->getBPosition());
   loadMatricesToShader();
   // draw the sphere
   ngl::VAOPrimitives::draw("cube");
   // draw the target points
-  ngl::ShaderLib::setUniform("Colour",1.0f,0.0f,0.0f,1.0f);
-  m_transform.setScale(0.1f,0.1f,0.1f);
+  ngl::ShaderLib::setUniform("Colour", 1.0f, 0.0f, 0.0f, 1.0f);
+  m_transform.setScale(0.1f, 0.1f, 0.1f);
   m_transform.setPosition(m_spring->getInitialAPosition());
   loadMatricesToShader();
   // draw the cube
   ngl::VAOPrimitives::draw("sphere");
-  ngl::ShaderLib::setUniform("Colour",0.0f,1.0f,0.0f,1.0f);
+  ngl::ShaderLib::setUniform("Colour", 0.0f, 1.0f, 0.0f, 1.0f);
 
-  m_transform.setScale(0.1f,0.1f,0.1f);
+  m_transform.setScale(0.1f, 0.1f, 0.1f);
   m_transform.setPosition(m_spring->getInitialBPosition());
   loadMatricesToShader();
 
@@ -160,28 +150,23 @@ void NGLScene::paintGL()
   ngl::VAOPrimitives::draw("sphere");
 }
 
-
-void NGLScene::timerEvent( QTimerEvent *)
+void NGLScene::timerEvent(QTimerEvent *)
 {
-	m_spring->update();
-	update();
+  m_spring->update();
+  update();
 }
 
 NGLScene::~NGLScene()
 {
-   killTimer(m_timer);
-
 }
 
 void NGLScene::startSimTimer()
 {
 
-m_timer=startTimer(m_timerValue);
+  m_timer = startTimer(m_timerValue);
 }
 
 void NGLScene::stopSimTimer()
 {
- killTimer(m_timer);
+  killTimer(m_timer);
 }
-
-
--- src/NGLSceneMouseControls.cpp
+++ src/NGLSceneMouseControls.cpp
@@ -1,68 +1,68 @@
 #include "NGLScene.h"
 #include <QMouseEvent>
-
-
+#include <QtGlobal>
 //----------------------------------------------------------------------------------------------------------------------
-void NGLScene::mouseMoveEvent( QMouseEvent* _event )
+void NGLScene::mouseMoveEvent(QMouseEvent *_event)
 {
-  // note the method buttons() is the button state when event was called
-  // that is different from button() which is used to check which button was
-  // pressed when the mousePress/Release event is generated
-  if ( m_win.rotate && _event->buttons() == Qt::LeftButton )
+// note the method buttons() is the button state when event was called
+// that is different from button() which is used to check which button was
+// pressed when the mousePress/Release event is generated
+#if QT_VERSION > QT_VERSION_CHECK(6, 0, 0)
+  auto position = _event->position();
+#else
+  auto position = _event->pos();
+#endif
+  if (m_win.rotate && _event->buttons() == Qt::LeftButton)
   {
-    int diffx = _event->x() - m_win.origX;
-    int diffy = _event->y() - m_win.origY;
-    m_win.spinXFace += static_cast<int>( 0.5f * diffy );
-    m_win.spinYFace += static_cast<int>( 0.5f * diffx );
-    m_win.origX = _event->x();
-    m_win.origY = _event->y();
-    update();
-  }
-  // right mouse translate code
-  else if ( m_win.translate && _event->buttons() == Qt::RightButton )
-  {
-    int diffX      = static_cast<int>( _event->x() - m_win.origXPos );
-    int diffY      = static_cast<int>( _event->y() - m_win.origYPos );
-    m_win.origXPos = _event->x();
-    m_win.origYPos = _event->y();
+    int diffx = position.x() - m_win.origX;
+    int diffy = position.y() - m_win.origY;
+    m_win.spinXFace += static_cast<int>(0.5f * diffy);
+    m_win.spinYFace += static_cast<int>(0.5f * diffx);
+    m_win.origX = position.x();
+    m_win.origY = position.y();
     update();
   }
 }
 
-
 //----------------------------------------------------------------------------------------------------------------------
-void NGLScene::mousePressEvent( QMouseEvent* _event )
+void NGLScene::mousePressEvent(QMouseEvent *_event)
 {
   // that method is called when the mouse button is pressed in this case we
-  // store the value where the maouse was clicked (x,y) and set the Rotate flag to true
-  if ( _event->button() == Qt::LeftButton )
+  // store the value where the mouse was clicked (x,y) and set the Rotate flag to true
+#if QT_VERSION > QT_VERSION_CHECK(6, 0, 0)
+  auto position = _event->position();
+#else
+  auto position = _event->pos();
+#endif
+
+  if (_event->button() == Qt::LeftButton)
   {
-    m_win.origX  = _event->x();
-    m_win.origY  = _event->y();
+    m_win.origX = position.x();
+    m_win.origY = position.y();
     m_win.rotate = true;
   }
   // right mouse translate mode
-  else if ( _event->button() == Qt::RightButton )
+  else if (_event->button() == Qt::RightButton)
   {
-    m_win.origXPos  = _event->x();
-    m_win.origYPos  = _event->y();
+    m_win.origXPos = position.x();
+    m_win.origYPos = position.y();
     m_win.translate = true;
   }
 }
 
 //----------------------------------------------------------------------------------------------------------------------
-void NGLScene::mouseReleaseEvent( QMouseEvent* _event )
+void NGLScene::mouseReleaseEvent(QMouseEvent *_event)
 {
+
   // that event is called when the mouse button is released
   // we then set Rotate to false
-  if ( _event->button() == Qt::LeftButton )
+  if (_event->button() == Qt::LeftButton)
   {
     m_win.rotate = false;
   }
   // right mouse translate mode
-  if ( _event->button() == Qt::RightButton )
+  if (_event->button() == Qt::RightButton)
   {
     m_win.translate = false;
   }
 }
-
